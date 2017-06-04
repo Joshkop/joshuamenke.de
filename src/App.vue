4eedfd9e1ed7b77faa2c24709fa1d17959eb1681
joshkop <j.menke@me.com>
2017-06-04 06:51:58 +0200

    finished MVP

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,320 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <div id="left">
 
-    <div class="ui instant move reveal">
-      <div class="visible content">
-        <img src="./assets/square-image.png" class="ui small image">
+      <a href="/" class="centered">
+        <h1>Joshua Menke</h1>
+        <img class="ui small circular image centered" src="https://unsplash.it/200" alt="Joshua Menke">
+      </a>
+      <div>
+        <div class="ui divider"></div>
+        <h2>Persönliches</h2>
+        <div class="ui divider"></div>
       </div>
-      <div class="hidden content">
-        <img src="./assets/chris.jpg" class="ui small image">
+
+      <div id="personal">
+        <div class="info">
+          <i class="birthday circular icon"></i>
+          <div>Geb. 13.10.1994</div>
+        </div>
+        <div class="info">
+          <i class="home circular icon"></i>
+          <a href="https://goo.gl/maps/JVHtcFxZz4q">
+            <div>Kortumstr. 30</div>
+            <div>44787 Bochum</div>
+          </a>
+        </div>
+        <div class="info">
+          <i class="text telephone circular icon"></i>
+          <a href="tel:+4917660890700">+49 176 - 60 89 0700</a>
+        </div>
+        <div class="info">
+          <i class="mail circular icon"></i>
+          <a href="mailto:info@joshuamenke.de">info@joshuamenke.de</a>
+        </div>
+        <div class="info">
+          <i class="browser circular icon"></i>
+          <a href="http://joshuamenke.de">joshuamenke.de</a>
+        </div>
+      </div>
+      <div>
+        <div class="ui divider"></div>
+        <h2>Sprachen</h2>
+        <div class="ui divider"></div>
+      </div>
+      <div id="sprachen">
+        <div class="info">
+          <span><b>Deutsch</b></span>
+          <span>Muttersprache</span>
+        </div>
+        <div class="info">
+          <span><b>Englisch</b></span>
+          <span>Fließend</span>
+        </div>
+      </div>
+      <div>
+        <div class="ui divider"></div>
+        <h2>Kenntnisse</h2>
+        <div class="ui divider"></div>
+      </div>
+      <div id="kenntnisse">
+        <div class="info">
+          <span><b>Python</b></span>
+          <div class="rating">
+            <img src="https://png.icons8.com/python-filled/ios7/100/" title="Python" width="13" height="13">
+            <img src="https://png.icons8.com/python-filled/ios7/100/" title="Python" width="13" height="13">
+            <img src="https://png.icons8.com/python-filled/ios7/100/" title="Python" width="13" height="13">
+            <img src="https://png.icons8.com/python-filled/ios7/100/" title="Python" width="13" height="13">
+            <img src="https://png.icons8.com/python/ios7/100/" title="Python" width="13" height="13">
+          </div>
+        </div>
+        <div class="info">
+          <span><b>Java</b></span>
+          <div class="rating">
+            <img src="https://png.icons8.com/java-bean-filled/ios7/100/" title="CSS" width="13" height="13">
+            <img src="https://png.icons8.com/java-bean-filled/ios7/100/" title="CSS" width="13" height="13">
+            <img src="https://png.icons8.com/java-bean/ios7/100/" title="CSS" width="13" height="13">
+            <img src="https://png.icons8.com/java-bean/ios7/100/" title="CSS" width="13" height="13">
+            <img src="https://png.icons8.com/java-bean/ios7/100/" title="CSS" width="13" height="13">
+          </div>
+        </div>
+        <div class="info">
+          <span><b>JavaScript</b></span>
+          <div class="rating">
+            <img src="https://png.icons8.com/javascript-filled/ios7/100/" title="Javascript" width="13" height="13">
+            <img src="https://png.icons8.com/javascript-filled/ios7/100/" title="Javascript" width="13" height="13">
+            <img src="https://png.icons8.com/javascript-filled/ios7/100/" title="Javascript" width="13" height="13">
+            <img src="https://png.icons8.com/javascript-filled/ios7/100/" title="Javascript" width="13" height="13">
+            <img src="https://png.icons8.com/javascript/ios7/100/" title="Javascript" width="13" height="13">
+          </div>
+        </div>
+        <div class="info">
+          <span><b>Html</b></span>
+          <div class="rating">
+            <img src="https://png.icons8.com/html-5-filled/ios7/100/" title="Html 5" width="13" height="13">
+            <img src="https://png.icons8.com/html-5-filled/ios7/100/" title="Html 5" width="13" height="13">
+            <img src="https://png.icons8.com/html-5-filled/ios7/100/" title="Html 5" width="13" height="13">
+            <img src="https://png.icons8.com/html-5-filled/ios7/100/" title="Html 5" width="13" height="13">
+            <img src="https://png.icons8.com/html-5/ios7/100/" title="Html 5" width="13" height="13">
+          </div>
+        </div>
+        <div class="info">
+          <span><b>CSS</b></span>
+          <div class="rating">
+            <img src="https://png.icons8.com/css-filled/ios7/100/" title="CSS" width="13" height="13">
+            <img src="https://png.icons8.com/css-filled/ios7/100/" title="CSS" width="13" height="13">
+            <img src="https://png.icons8.com/css-filled/ios7/100/" title="CSS" width="13" height="13">
+            <img src="https://png.icons8.com/css/ios7/100/" title="CSS" width="13" height="13">
+            <img src="https://png.icons8.com/css/ios7/100/" title="CSS" width="13" height="13">
+          </div>
+        </div>
+        <div class="ui divider"></div>
+        <div class="info">
+          <span><b>MS Office</b></span>
+          <div class="rating">
+            <img src="https://png.icons8.com/microsoft-excel-filled/ios7/100/" title="Microsoft Office" width="13" height="13">
+            <img src="https://png.icons8.com/microsoft-excel-filled/ios7/100/" title="Microsoft Office" width="13" height="13">
+            <img src="https://png.icons8.com/microsoft-excel-filled/ios7/100/" title="Microsoft Office" width="13" height="13">
+            <img src="https://png.icons8.com/microsoft-excel-filled/ios7/100/" title="Microsoft Office" width="13" height="13">
+            <img src="https://png.icons8.com/microsoft-excel/ios7/100/" title="Microsoft Office" width="13" height="13">
+          </div>
+        </div>
+        <div class="info">
+          <span><b>Linux</b></span>
+          <div class="rating">
+            <img src="https://png.icons8.com/linux-filled/ios7/100/" title="Microsoft Office" width="13" height="13">
+            <img src="https://png.icons8.com/linux-filled/ios7/100/" title="Microsoft Office" width="13" height="13">
+            <img src="https://png.icons8.com/linux-filled/ios7/100/" title="Microsoft Office" width="13" height="13">
+            <img src="https://png.icons8.com/linux/ios7/100/" title="Microsoft Office" width="13" height="13">
+            <img src="https://png.icons8.com/linux/ios7/100/" title="Microsoft Office" width="13" height="13">
+
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="ui divider"></div>
+        <h2>Interessen & Hobbies</h2>
+        <div class="ui divider"></div>
+      </div>
+      <div class="hobbies">
+          <div class="info"><b>Computer / Programmieren</b></div>
+          <div  class="info"><b>Elektronische Musik</b></div>
+          <div  class="info"><b>Kochen</b></div>
+      </div>
+      <div>
+        <div class="ui divider"></div>
+        <h2>Links</h2>
+        <div class="ui divider"></div>
+      </div>
+      <div class="links">
+        <div class="info">
+          <i class="github circular icon"></i>
+          <a href="http://www.github.com/Joshkop">github.com/Joshkop</a>
+        </div>
+        <div class="info">
+          <i class="linkedin circular icon"></i>
+          <a href="http://www.linkedin.com/in/Joshkop">linkedin.com/in/Joshkop</a>
+        </div>
+        <div class="info">
+          <i class="facebook circular icon"></i>
+          <a href="https://www.facebook.com/Joshkop">facebook.com/Joshkop</a>
+        </div>
+        <div class="info">
+          <i class="twitter circular icon"></i>
+          <a href="https://www.twitter.com/_Joshkop">twitter.com/_Joshkop</a>
+        </div>
       </div>
     </div>
-
-    <div class="ui inverted segment">
-      <div class="ui inverted accordion">
-        <div class="active title">
-          <i class="dropdown icon"></i>
-          What is a dog?
+    <div id="right">
+      <div>
+        <div class="ui divider"></div>
+        <h2>Berufserfahrung</h2>
+        <div class="ui divider"></div>
+        <div id="berufe">
+          <div class="beruf">
+            <h3>Programmierer und Datenerfasser</h3>
+            <div class="info bold italic">
+              <span>Fachverlag für Verbraucherinformationen</span>
+              <span>08/2015 - 01/2017</span>
+            </div>
+            <div class="text">Sammeln und bearbeiten von Daten sowie die Programmierung von Datenverarbeitungsskripts und Eingabeoberflächen.</div>
+          </div>
+          <div class="beruf">
+            <h3>Technik AG</h3>
+            <div class="info bold italic">
+              <span>Grundschule Bochum Westenfeld</span>
+              <span>04/2013 - 02/2014</span>
+            </div>
+            <div class="text">Mitleitung der Technik AG, in der Grundschülern die Welt der Technik
+näher gebracht werden soll. Dabei unterrichte Ich mit einem weiteren Schüler
+diese Grundschüler und lies diese Experimente unter unserer Beobachtung
+und Leitung durchführen.</div>
+          </div>
+          <div class="beruf">
+            <h3>Verkäufer</h3>
+            <div class="info bold italic">
+              <span>Edmund Goebgens Stadion- und Veranstaltungsservice GmbH</span>
+              <span>03/2012 - 06/2012</span>
+            </div>
+            <div class="text">Verkauf von Lebensmitteln, vor allem Eis und Bier, in Stadien wie der Veltins Arena in Gelsenkirchen oder Lanxess Arena in Köln. Dabei verkaufte ich hauptsächlich in den Sitzplatzreihen mit Bauchladen.</div>
+          </div>
+          <div class="beruf">
+            <h3>Praktikum im Bereich IT</h3>
+            <div class="info bold italic">
+              <span>Faber Datentechnik</span>
+              <span>11/2011 - 11/2011</span>
+            </div>
+            <div class="text">Praktikum vom 07.11 – 18.11.11 im Bereich IT. Dabei vor allem Einführung in
+die Welt der Hardware und Montage von Computern.</div>
+          </div>
+          <div class="beruf">
+            <h3>Praktikum im Bereich IT</h3>
+            <div class="info bold italic">
+              <span>DOGEWO21</span>
+              <span>02/2010 - 02/2010</span>
+            </div>
+            <div class="text">Praktikum vom 22.02 – 26.02.10 im Bereich IT. Dabei vor allem Software
+Lösungen und Fehlerbehebung.</div>
+          </div>
         </div>
-        <div class="active content">
-          <p>A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.</p>
-        </div>
-        <div class="title">
-          <i class="dropdown icon"></i>
-          What kinds of dogs are there?
-        </div>
-        <div class="content">
-          <p>There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.</p>
-        </div>
-        <div class="title">
-          <i class="dropdown icon"></i>
-          How do you acquire a dog?
-        </div>
-        <div class="content">
-          <p>Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.</p>
-          <p>A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.</p>
+        <div class="ui divider"></div>
+        <h2>Bildungsweg</h2>
+        <div class="ui divider"></div>
+        <div id="bildung">
+          <div class="bildung">
+            <h3>Bachelor of Science (B.Sc.), Wirtschaftsinformatik</h3>
+            <div class="info bold italic">
+              <span>Universität Duisburg-Essen, Standort Essen</span>
+              <span>10/2014 - 02/2017</span>
+            </div>
+            <div class="text">Vorzeitig Beendet.</div>
+          </div>
+          <div class="bildung">
+            <h3>Abitur</h3>
+            <div class="info bold italic">
+              <span>Hellwegschule Bochum</span>
+              <span>02/2010 - 05/2014</span>
+            </div>
+            <div class="text">Abschlussnote: 2,8</div>
+          </div>
+          <div class="bildung">
+            <h3>Gymnasium</h3>
+            <div class="info bold italic">
+              <span>Internatsschule Schloss Neubeuern, Bayern</span>
+              <span>09/2005 - 02/2010</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 
-export default {
-  name: 'app',
-  components: {
-    Hello
-  },
-}
+
+  export default {
+    name: 'app'
+  }
 
 
 </script>
 
 <style lang="sass">
-  @import "~styles/border"
-</style>
+  body
+    background: rgba(144, 238, 144, 0.28) !important
+  #app
+    display: flex
+    justify-content: center
+    /*min-height: 100%*/
+    padding: 30px
+  #left
+    padding: 30px
+    background-color: lightgreen
+    min-height: 100%
+    display: flex
+    flex-direction: column
+  #right
+    width: 650px
+    padding: 30px
+    background-color: whitesmoke
+    min-height: 100%
+    display: flex
+    flex-direction: column
+  .centered
+    align-self: center
+    text-align: center
+  .bold
+    font-weight: bold
+  .italic
+    font-style: italic
+  .info
+    display: flex
+    justify-content: space-between
+    align-items: center
+    padding-top: 3px
+    padding-bottom: 3px
+  .rating
+    display: flex
+    justify-content: space-around
+    img
+      margin-left: 2px
+  .inverted
+    filter: invert(100%)
+  footer
+    background-color: rgba(144, 238, 144, 0.64)
+    position: absolute
+    padding: 5px
+    bottom: 0
+    left: 0
+    right: 0
+  .beruf
+    margin-bottom: 20px
+  .bildung
+    margin-bottom: 20px
+  h3
+    margin-bottom: 5px !important
+    color: green
+  #berufe
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
